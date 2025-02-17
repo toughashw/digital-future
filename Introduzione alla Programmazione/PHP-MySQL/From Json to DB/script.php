e462491ed15d386b6f0a172e3284ea7784b54cc9
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2025-02-17 16:31:02 +0000

    from json to db

--- Introduzione alla Programmazione/PHP-MySQL/From Json to DB/script.php
+++ Introduzione alla Programmazione/PHP-MySQL/From Json to DB/script.php
@@ -3,7 +3,7 @@
 $host = '';
 $username = '';  // Usa il tuo nome utente MySQL
 $password = '';      // Usa la tua password MySQL
-$dbname = '';    // Usa il tuo nome di database
+$dbname = ''; // Usa il tuo nome di database
 
 // Connessione al database MySQL
 $conn = new mysqli($host, $username, $password, $dbname);
@@ -19,17 +19,17 @@ $data = json_decode($jsonData, true); // Decodifica il JSON in un array associat
 
 // Preparare la query SQL per inserire i dati nel database
 $stmt = $conn->prepare("INSERT INTO utenti (nome, cognome, email) VALUES (?, ?, ?)");
-$stmt->bind_param("ss", $name, $email); // "ss" indica che i parametri sono stringhe
+$stmt->bind_param("ssi", $nome, $cognome, $email); // "ssi" indica che il primo e secondo parametro sono stringhe, il terzo è un intero
 
 // Itera attraverso i dati e inseriscili nel database
 foreach ($data as $user) {
     $nome = $user['nome'];
     $cognome = $user['cognome'];
-    $email = $user['email'];
-    
+    $email = $user['email'];  // Assicurati che 'anni' sia un valore numerico
+
     // Esegui la query di inserimento
     if ($stmt->execute()) {
-        echo "Dati inseriti con successo: $name, $cognome, $email\n";
+        echo "Dati inseriti con successo: $nome, $cognome, $email\n";
     } else {
         echo "Errore nell'inserimento dei dati: " . $stmt->error . "\n";
     }
